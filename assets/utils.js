e90c99d326e6b959746a16eeeff42d8ff95d8aa2
Darren <d-pierre90@hotmail.com>
2019-09-16 11:52:43 -0400

    basic structure of french implementation

--- assets/utils.js
+++ assets/utils.js
@@ -15,4 +15,32 @@ function getParams() {
     return params;
 }
 
+function determine_language(){
+      
 
+    let x= document.getElementById('language_button')
+    if (x.innerHTML === "English") {
+        return 'en'
+    } else {
+        return 'fr'
+    }
+
+}
+
+function toggleLangButton(){
+ console.log("hello world")
+    let x= document.getElementById('language_button')
+    if (x.innerHTML === "English") {
+        x.innerHTML="Francais"
+    } else {
+        x.innerHTML="English"
+    }
+
+
+}
+function changeLang(){
+   console.log("hello world")
+    toggleLangButton()
+     $(".lang_term").hide();
+     $("." + determine_language()).show();
+}; 
